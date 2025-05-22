e90fb450754c043a47ff33d578cdec0ff1427657
yipin <1259478761@qq.com>
2025-05-22 16:11:13 +0800

    add a script for test

---
+++ start_server.sh
@@ -0,0 +1,7 @@
+
+export TORCHINDUCTOR_CACHE_DIR=~/.triton
+CUDA_VISIBLE_DEVICES=7 python3 -m sglang.launch_server \
+    --model-path OpenGVLab/InternVL3-1B \
+    --trust-remote-code \
+    --chat-template internvl-2-5 \
+    --mem-fraction-static 0.2
