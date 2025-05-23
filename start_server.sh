b9e713d773bd41833d9bda2b8e024989aa4fbc2f
yipin <1259478761@qq.com>
2025-05-23 17:19:50 +0800

    update patch_pruning and token pruning verify code

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,20 @@
 
 export TORCHINDUCTOR_CACHE_DIR=~/.triton
-CUDA_VISIBLE_DEVICES=5 python3 -m sglang.launch_server \
+CUDA_VISIBLE_DEVICES=4 python3 -m sglang.launch_server \
     --model-path OpenGVLab/InternVL3-1B \
     --trust-remote-code \
     --chat-template internvl-2-5 \
     --mem-fraction-static 0.2 \
-    --token-pruning-alg patch-pruning \
+    --token-pruning-alg tome \
     --token-pruning-ratio 0.5
+
+
+
+# export TORCHINDUCTOR_CACHE_DIR=~/.triton
+# CUDA_VISIBLE_DEVICES=4 python3 -m sglang.launch_server \
+#     --model-path Qwen/Qwen2.5-VL-3B-Instruct \
+#     --trust-remote-code \
+#     --chat-template qwen2-vl \
+#     --mem-fraction-static 0.2 \
+#     --token-pruning-alg tome \
+#     --token-pruning-ratio 0.5
